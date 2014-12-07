5b93cd96df5693ae2a99ca557267fac3c9bcd8b6
mert.rgun <mert.rgun@gmail.com>
2014-12-07 11:14:11 +0000

    Login implementation

--- WeCalEvent/src/main/java/com/se2/wecalevent/controllers/UserLoginView.java
+++ WeCalEvent/src/main/java/com/se2/wecalevent/controllers/UserLoginView.java
@@ -5,51 +5,56 @@
  */
 package com.se2.wecalevent.controllers;
 
- 
+import com.se2.wecalevent.entities.User;
+import com.se2.wecalevent.remote.sessionBeanRemote;
+import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
- 
+
 import org.primefaces.context.RequestContext;
- 
+
 @ManagedBean
+@SessionScoped
 public class UserLoginView {
-     
+
+    @EJB
+    private sessionBeanRemote ejb;
+
     private String email;
-     
+
     private String password;
- 
+
     public String getEmail() {
         return email;
     }
- 
+
     public void setEmail(String email) {
         this.email = email;
     }
- 
+
     public String getPassword() {
         return password;
     }
- 
+
     public void setPassword(String password) {
         this.password = password;
     }
-   
+
     public String login() {
         RequestContext context = RequestContext.getCurrentInstance();
         FacesMessage message = null;
-        boolean loggedIn = false;
-         
-        if(email != null && email.equals("admin") && password != null && password.equals("admin")) {
-            loggedIn = true;
-            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", email);
-        } else {
-            loggedIn = false;
-            message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Loggin Error", "Invalid credentials");
+        User theUser = ejb.loginUser(email, password);
+        boolean loggedIn = theUser != null;
+        if (loggedIn) {
+            message = new FacesMessage("Hello", theUser.getName());
         }
-         
+        else {
+            message = new FacesMessage("I'm sorry :(");
+        }
         FacesContext.getCurrentInstance().addMessage(null, message);
         context.addCallbackParam("loggedIn", loggedIn);
         return "home";
-    }   
+    }
 }
---
+++ WeCalEvent/src/main/java/com/se2/wecalevent/remote/sessionBeanRemote.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.se2.wecalevent.remote;
+
+import com.se2.wecalevent.entities.User;
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author Mert
+ */
+@Remote
+public interface sessionBeanRemote {
+    public User loginUser(String email, String password);
+}
