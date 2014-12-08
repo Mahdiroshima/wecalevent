1689042ef415e8830760aad8c1af3ae7db82caa3
mert.rgun@gmail.com <mert.rgun@gmail.com@adac6981-282d-c8c2-1a56-3d72320bb25d>
2014-12-08 00:34:51 +0000

    Register implementation but having an error which is not fixed yet

--- trunk/WeCalEvent/src/main/java/com/se2/wecalevent/ejb/sessionBean.java
+++ trunk/WeCalEvent/src/main/java/com/se2/wecalevent/ejb/sessionBean.java
@@ -7,12 +7,10 @@ package com.se2.wecalevent.ejb;
 
 import com.se2.wecalevent.entities.User;
 import com.se2.wecalevent.remote.sessionBeanRemote;
-import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import org.eclipse.persistence.internal.jpa.EntityManagerImpl;
 
 /**
  *
@@ -24,6 +22,7 @@ public class sessionBean implements sessionBeanRemote {
     @PersistenceContext(unitName = "com.se2_WeCalEvent_war_1.0-SNAPSHOTPU")
     private EntityManager entityManager;
 
+    @Override
     public User loginUser(String email, String password) {
         Query query = entityManager.createNamedQuery("User.findByEmail");
         query.setParameter("email", email);
@@ -36,4 +35,23 @@ public class sessionBean implements sessionBeanRemote {
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+
+    
+    @Override
+    public boolean register(String email, String password, String calendar, String name, String surname) {
+        Query query = entityManager.createNamedQuery("User.findByEmail");
+        query.setParameter("email", email);
+        User loggedin = (User) query.getSingleResult();
+        if (loggedin != null) {
+            return false;
+        }
+        User newUser = new User();
+        newUser.setEmail(email);
+        newUser.setName(name);
+        newUser.setSurname(surname);
+        newUser.setPass(password);
+        newUser.setCalendar(calendar);
+        entityManager.persist(newUser);
+        return true;
+    }
 }
