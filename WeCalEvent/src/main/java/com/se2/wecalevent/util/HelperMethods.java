ab21fc766b30c9f47856e0f363087c73bf45e9a2
NazrinCVD <NazrinCVD@gmail.com>
2014-12-20 19:37:30 +0000

    Add helper method to the helpermethods class

--- WeCalEvent/src/main/java/com/se2/wecalevent/util/HelperMethods.java
+++ WeCalEvent/src/main/java/com/se2/wecalevent/util/HelperMethods.java
@@ -10,5 +10,23 @@ package com.se2.wecalevent.util;
  * @author Mert Ergun <mert.rgun@gmail.com>
  */
 public class HelperMethods {
-    
+    /**
+     * This method edits the name string like below:
+     * mErT jR eRgUn -> Mert Jr Ergun
+     * @param name The string that is going to be edited
+     * @return Resulting String
+     */
+    public static String prettifyName(String name) {
+        //Seperate words
+        String[] names = name.split(" ");
+        String result = "";
+        for (String ele : names) {
+            if (ele.length() > 0)  {
+                //merge the seperated words
+                result += ele.substring(0,1).toUpperCase() + ele.substring(1).toLowerCase();
+            }
+        }
+        //return resulting string
+        return result;
+    }
 }
